0297c2ebb00974738d9cfe88adc00ea8e826dbd2
Aryan <arayn.32@gmail.com>
2022-11-28 01:53:45 +0530

    Finished printcommands.py

---
+++ class3-printcommand-cipherschools.py
@@ -0,0 +1,14 @@
+print("jatin katyal")
+
+print(1)
+
+print(1,2,3,4,5,6,sep="-")
+
+print(1,2,3,"jatin",3.4,1+5j,sep=",")
+
+print("a",end=")")
+print("b")
+print("c")
+
+print("Hello",end=";")
+print("World !")
